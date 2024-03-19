61501d7d24644db249353b979dcfb598d4206445
Alexander Betancourt <AlexanderBBetancourt@proton.me>
2024-03-19 02:37:36 -0700

    InitCommit

---
+++ Tower Defense Unity Project/Assets/Scripts/WaveSpawner.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+using System.Collections;
+using TMPro;
+public class WaveSpawner : MonoBehaviour
+{
+    public Transform enemyPrefab;
+    public Transform spawnPoint;
+    public float timeBetweenWaves = 5f;
+    public TextMeshProUGUI waveCountdownText;
+    private float countdown = 2f;
+    private int waveIndex = 0;
+
+    void Update()
+    {
+        if (countdown <= 0f)
+        {
+            StartCoroutine(SpawnWave());
+            countdown = timeBetweenWaves;
+        }
+
+        countdown -= Time.deltaTime; 
+        string str = string.Format("Next wave in : {0}", Mathf.Round(countdown));
+
+        waveCountdownText.text = str;
+    }
+
+    IEnumerator SpawnWave()
+    {
+        waveIndex++;
+
+        for (int i = 0; i < waveIndex; i++)
+        {
+            SpawnEnemy();
+            yield return new WaitForSeconds(0.5f);
+        }
+    }
+
+    void SpawnEnemy()
+    {
+        Instantiate(enemyPrefab, spawnPoint.position, spawnPoint.rotation);
+    }
+}
